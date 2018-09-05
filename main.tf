c5ac21770dce1c52381fc64ebe675bd748db6ab9
denislavdenov <42144642+denislavdenov@users.noreply.github.com>
2018-09-05 09:31:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,16 +10,16 @@ provider "github" {
 }
 
 resource "github_repository" "example" {
-  name        = "example1"
+  name        = "example"
   description = "My awesome ghrepo"
 }
 
-resource "random_pet" "repo" {
-  length    = "4"
-  separator = "-"
-}
+#resource "random_pet" "repo" {
+ # length    = "4"
+  #separator = "-"
+#}
 
-resource "github_repository" "example2" {
-  name        = "${random_pet.repo.id}"
-  description = "My awesome pet project"
-}
+#resource "github_repository" "example2" {
+ # name        = "${random_pet.repo.id}"
+  #description = "My awesome pet project"
+#}
