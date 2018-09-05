82deb0bd5a5e9065fea42e44376cadfc246532ea
denislavdenov <42144642+denislavdenov@users.noreply.github.com>
2018-09-05 09:34:09 +0300

    Update check_repo_spec.rb

--- test/integration/default/controls/check_repo_spec.rb
+++ test/integration/default/controls/check_repo_spec.rb
@@ -1,6 +1,6 @@
 control 'check_website' do
 
-  describe http('https://github.com/VisitBG/example-kitchen') do
+  describe http('https://github.com/DarkMagics/example-kitchen') do
     its('status') { should cmp 200 }
     its('body') { should match 'My awesome ghrepo' }
   end
